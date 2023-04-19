878b57163c96a221179b230fcf9d47c8a01e9a61
12343w <131249131+12343w@users.noreply.github.com>
2023-04-19 17:34:34 +0530

    Create average.py

---
+++ average.py
@@ -0,0 +1,9 @@
+# Python Program to Find Average of Two Numbers
+# First Number
+a = 5
+# Second Number
+b = 10
+# Calculating average of two numbers
+avg = (a + b) / 2
+# Display output
+print("Average of two numbers = %.2f" %avg)
